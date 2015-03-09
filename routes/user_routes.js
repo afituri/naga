b8dec347ca8b281668b276bf00a798cf5a64603b
faroukshouba <faroukshouba@gmail.com>
2015-03-09 19:34:26 +0200

    edit

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -16,11 +16,11 @@ router.get('/department', function(req, res) {
   res.render('department', { title: ' department' });
 });
 
-<<<<<<< HEAD
+
 router.get('/account', function(req, res) {
   res.render('account', { title: ' my account' });
 });
-=======
+
     
     router.get('/abdo', function(req,res){
       userMgr.deleteById(1,function(result){
@@ -28,9 +28,6 @@ router.get('/account', function(req, res) {
          res.render('abdo',{title: 'abdo'});
          });
       });
-
-
->>>>>>> dacd9c108ea3469529b59d32bf09745bb79c8d83
 
 // set a cookie to requested locale
 router.get('/:locale', function (req, res) {

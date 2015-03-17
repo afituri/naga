cda358182e4000bcd7a54f0df7cb17a259a7dd6c
aladdin <alaaden300@gmail.com>
2015-03-17 23:10:28 +0200

    Edit for some pages

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -14,21 +14,21 @@ router.get('/adminPage', function(req, res) {
   res.render('adminPage', { title: 'Admin Page' });
 });
 
-router.get('/testBlockPage', function(req, res) {
-  res.render('testBlockPage', { title: 'test Block Page' });
+router.get('/adminTest', function(req, res) {
+  res.render('adminTest', { title: 'Admin Test' });
 });
 
 router.get('/adminRegUsers', function(req, res) {
-  res.render('adminRegUsers', { title: 'Admin Page' });
+  res.render('adminRegUsers', { title: 'Admin Register Users' ,collapseIn:"in", chosenRU:"chosen"});
 });
 
 router.get('/adminShowUsers', function(req, res) {
-  res.render('adminShowUsers', { title: 'Admin Page' ,username:users });
+  res.render('adminShowUsers', { title: 'Admin Show Users' ,collapseIn:"in",chosenSU:"chosen",username:users });
 });
 
 
 router.get('/adminSerialNumber', function(req, res) {
-  res.render('adminSerialNumber', { title: 'Admin Page' });
+  res.render('adminSerialNumber', { title: 'Admin Serial Number' });
 });
 
 

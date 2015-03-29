2b9a669938859b87470416208681185d49d5f4b9
Ahmed Fituri <ahmed.elfituri@gmail.com>
2015-03-29 18:52:10 +0200

    updating batch1

--- batch1.sql
+++ batch1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `customer` ADD `mobile` VARCHAR(30) NULL , ADD `phone` VARCHAR(30) NULL ;
 ALTER TABLE `city` ADD `status` TINYINT(1) NULL DEFAULT '1' AFTER `name_en`;
+ALTER TABLE `company` ADD `name_en` VARCHAR(200) NULL AFTER `name`;

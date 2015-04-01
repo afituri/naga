3a5aec5f2c3328ea1d205bb927cff6a759ce894c
FaisalRahil <faisallrahil@gmail.com>
2015-04-01 19:52:19 +0200

    add city

--- public/js/adminCities.js
+++ public/js/adminCities.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
-  
+  $('body').on('click', '#sub', function () {
+    $('#formCitie').submit();
+  });
   $('body').on('submit', '#formCitie', function(data) {
     $.post("/addcity", $("form").serializeObject(), function(data, textStatus, jqXHR){
-      alert(data);
       if(data !=true){
         for (err in data) {
           $("#err").append('<h1>'+data[err].msg+'</h1>');
         }
+      alert(data);
+
       }else{
         window.location.href="/adminCities";
 

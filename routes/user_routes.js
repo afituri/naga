342448ccc50e7dfd16f12d2c53fa37d9cf72447a
faroukshouba <faroukshouba@gmail.com>
2015-04-02 09:06:26 +0200

    first pull

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -15,6 +15,26 @@ router.get('/department', function(req, res) {
   res.render('department', { title: ' department' });
 });
 
+router.get('/myAcount', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('myAcount', { title: 'myAcount' });
+});
+
+router.get('/forgotPassword?', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('forgotPassword', { title: 'myAcount' });
+});
+
+router.get('/newPassword?', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('newPassword', { title: 'myAcount' });
+});
+
+
+router.get('/login', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('login', { title: 'login' });
+});
 
 router.get('/account', function(req, res) {
   res.render('account', { title: ' my account' });

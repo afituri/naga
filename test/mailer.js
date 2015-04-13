f07888490ed2d360502841baeab5f813bff5c277
Ahmed Fituri <ahmed.elfituri@gmail.com>
2015-04-13 20:47:53 +0200

    applying email api

---
+++ test/mailer.js
@@ -0,0 +1,25 @@
+mailer = require('../app/mailer');
+
+
+var obj = {
+  template : "activate_seller",
+  subject : "Naga account activation",
+  locals : {
+    email : "ahmed.elfituri@gmail.com",
+    user : {
+      name : "Ahmed Fituri",
+      link : "http://naga.ly/activate/SDFSDKLJHDFSSLDKF23424N234L234H234"
+    }
+  }
+}
+
+
+describe('#SendActivationMail()',function(){
+  it('Should send an email without an error', function(done){
+
+    mailer.send(obj,function(err){
+      if (err) throw err;
+      done();
+    });
+  });
+})

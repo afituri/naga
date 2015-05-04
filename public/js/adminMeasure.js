591e857464db67c1e9e2947282fba7645de61a8f
FaisalRahil <faisallrahil@gmail.com>
2015-05-04 19:41:17 +0200

    fexed add

--- public/js/adminMeasure.js
+++ public/js/adminMeasure.js
@@ -34,47 +34,70 @@ $(document).ready(function(){
     window.location.href='/sizes/'+$(this).val();
   });
 
+  $("#formMeasure").validate({
+    rules:{
+      name:{
+        required: true,
+      },
+      name_en:{
+        required : true,
+      },
+    },
+    messages:{
+      name:{
+        required: "Please enter Measure in arabic !",
+      },
+      name_en:{
+        required: "Please enter Measure in english !",
+      },
+    },
+  });
 
   $("#formMeasure").submit(function() {
-    $.post("/addMeasure", $("form").serializeObject(), function(data, error){
-      if(data.stat !=true){
-        // $("#err").empty();
-        // for (err in data.result) {
-        //   $("#err").append('<h1>'+data.result[err].msg+'</h1>');
-        // }
-      } 
-      else {
-        if($("#MeasureTable").children().length>=10){
-          $("#MeasureTable tr:last-child").remove();
-        }
-        $("#MeasureTable").prepend('<tr data-id="'+data.result[0].idmeasure+'">'+
-        '<td class="text-center"><a id="name'+data.result[0].idmeasure+'" href="#" data-type="text" data-pk="'+data.result[0].idmeasure+'" class="editable editable-click editable-disabled">'+data.result[0].name+'</a></td>'+
-        '<td class="text-center"><a id="name_en'+data.result[0].idmeasure+'" href="#" data-type="text" data-pk="'+data.result[0].idmeasure+'" class="editable editable-click editable-disabled">'+data.result[0].name_en+'</a></td>'+
-        '<td class="text-center"><button id="view" value="'+data.result[0].idmeasure+'" data-title="Edit" data-toggle="modal" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button>'+
-        '</td><td class="text-center"><button id="enable" value="'+data.result[0].idmeasure+'" data-title="Edit" data-toggle="modal" class="btn btn-info btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>'+
-        '</td><td class="text-center"><button id="delete" href="#del" data-toggle="modal" value="'+data.result[0].idmeasure+'" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button></td></tr>');
+    var isvalidate=$("#formMeasure").valid();
+    if(isvalidate){
+      $.post("/addMeasure", $("form").serializeObject(), function(data, error){
+        if(data.stat !=true){
+          // $("#err").empty();
+          // for (err in data.result) {
+          //   $("#err").append('<h1>'+data.result[err].msg+'</h1>');
+          // }
+        } 
+        else {
+          $('#name').val("");
+          $('#name_en').val("");
+          if($("#MeasureTable").children().length>=10){
+            $("#MeasureTable tr:last-child").remove();
+          }
+          $("#MeasureTable").prepend('<tr data-id="'+data.result[0].idmeasure+'">'+
+          '<td class="text-center"><a id="name'+data.result[0].idmeasure+'" href="#" data-type="text" data-pk="'+data.result[0].idmeasure+'" class="editable editable-click editable-disabled">'+data.result[0].name+'</a></td>'+
+          '<td class="text-center"><a id="name_en'+data.result[0].idmeasure+'" href="#" data-type="text" data-pk="'+data.result[0].idmeasure+'" class="editable editable-click editable-disabled">'+data.result[0].name_en+'</a></td>'+
+          '<td class="text-center"><button id="view" value="'+data.result[0].idmeasure+'" data-title="Edit" data-toggle="modal" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button>'+
+          '</td><td class="text-center"><button id="enable" value="'+data.result[0].idmeasure+'" data-title="Edit" data-toggle="modal" class="btn btn-info btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>'+
+          '</td><td class="text-center"><button id="delete" href="#del" data-toggle="modal" value="'+data.result[0].idmeasure+'" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button></td></tr>');
 
-        $('#newMeasure').modal('hide');
-        $.notify({
-          title: "<strong>Successful:</strong> ",
-          message: "Add a new Measure has successfully"
-        },{
-          type: 'success',
-          allow_dismiss: true,
-          showProgressbar: false,
-          placement: {
-            from: 'top',
-            align: 'center'
-          },
-          mouse_over: null,
-          newest_on_top: true,
-          animate: {
-            enter: 'animated flipInY',
-            exit: 'animated flipOutX'
-          },
-        });
-      }
-    });
+          $('#newMeasure').modal('hide');
+          $.notify({
+            title: "<strong>Successful:</strong> ",
+            message: "Add a new Measure has successfully"
+          },{
+            type: 'success',
+            allow_dismiss: true,
+            showProgressbar: false,
+            placement: {
+              from: 'top',
+              align: 'center'
+            },
+            mouse_over: null,
+            newest_on_top: true,
+            animate: {
+              enter: 'animated flipInY',
+              exit: 'animated flipOutX'
+            },
+          });
+        }
+      });
+    }
     return false;
   });
 });

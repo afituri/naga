3250f57576430c1ceb955730f8dbeb423ba5ebfe
Ahmed Fituri <ahmed.elfituri@gmail.com>
2015-05-05 19:56:24 +0200

    substracting the item table

--- batch1.sql
+++ batch1.sql
@@ -16,7 +16,33 @@ CREATE TABLE IF NOT EXISTS `naga`.`brand` (
   PRIMARY KEY (`idbrand`))
 ENGINE = InnoDB;
 
-ALTER TABLE `item` ADD `item_desc` TEXT NOT NULL AFTER `discount_flag`;
 ALTER TABLE `item_has_size` DROP `color_idcolor`;
 ALTER TABLE `item_has_size` ADD `quantity` INT NULL ;
 ALTER TABLE `item` ADD `color_idcolor` INT NULL AFTER `brand_idbrand`, ADD `quantity` INT NOT NULL DEFAULT '0' AFTER `color_idcolor`;
+
+
+-- -----------------------------------------------------
+-- Table `naga`.`item_core`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `naga`.`item_core` (
+  `iditem_core` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(500) NOT NULL,
+  `name_en` VARCHAR(500) NULL,
+  `company_idcompany` INT NOT NULL,
+  `tog_idtog` INT NOT NULL,
+  `genre_idgenre` INT NOT NULL,
+  `tob_idtob` INT NOT NULL,
+  `item_desc` TEXT NULL,
+  `status` TINYINT(1) NULL DEFAULT 1,
+  `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` DATETIME NULL,
+  `brand_idbrand` INT NOT NULL,
+  `admin_idadmin` INT NOT NULL,
+  PRIMARY KEY (`iditem_core`))
+ENGINE = InnoDB;
+
+ALTER TABLE `item` ADD `item_core_iditem_core` BIGINT NOT NULL AFTER `quantity`;
+
+
+ALTER TABLE `item` DROP `name`, DROP `name_en`, DROP `item_desc`, DROP `company_idcompany`, DROP `tog_idtog`, DROP `genre_idgenre`, DROP `tob_idtob`, DROP `brand_idbrand`;
+

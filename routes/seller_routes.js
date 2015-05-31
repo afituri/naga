bf554c618965a20d2dd8cc234ac1ab3557e0a4b0
Aladdin <alaaden3000@gmail.com>
2015-05-31 19:15:55 +0200

    add new Tmplate for user

--- routes/seller_routes.js
+++ routes/seller_routes.js
@@ -1,17 +1,55 @@
 var express = require('express');
 var router = express.Router();
 var i18n = require('../app/i18n');
-var uploadManager = require('../app/upload')(router);
+var userMgr= require('../app/user_app').userMgr;
+var CityMgr = require('../app/city').CityMgr;
 
-/* GET home page. */
+/* GET home page. */ 
+
 router.get('/', function(req, res) {
   i18n.setlang(req,res);
-  res.render('login', { title: 'Seller Login' });
+  res.render('homeUser', { title: 'homeUser' });
 });
 
-router.get('/index', function (req, res) {
-  res.render('index', { title: 'Seller Dashboard' });
+router.get('/department', function(req, res) {
+  res.render('department', { title: ' department' });
 });
+
+router.get('/myAcount', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('myAcount', { title: 'myAcount' });
+});
+router.get('/editProfile', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('editProfile', { title: 'editProfile' });
+});
+
+router.get('/forgotPassword', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('forgotPassword', { title: 'myAcount' });
+});
+
+
+router.get('/login', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('login', { title: 'login' });
+});
+
+router.get('/account', function(req, res) {
+  res.render('account', { title: ' My Account' });
+});
+
+router.get('/myOrder', function(req, res) {
+  res.render('myOrder', { title: 'myOrder' });
+});
+
+
+
+router.get('/abdo', function(req,res){
+  userMgr.deleteById(1,function(result){
+     res.render('abdo',{title: 'abdo'});
+     });
+  });
 
 router.get('/homePage', function(req, res) {
   i18n.setlang(req,res);
@@ -23,39 +61,20 @@ router.get('/products', function(req, res) {
   res.render('products', { title: 'Seller' });
 });
 
-router.get('/compair', function(req, res) {
-  i18n.setlang(req,res);
-  res.render('compair', { title: 'Seller' });
-});
+router.get('/location', function(req, res) {
+  res.render('location', { title: ' location' });
+}); 
 
-router.get('/components', function(req, res) {
-  i18n.setlang(req,res);
-  res.render('components', { title: 'Seller' });
-});
 
-router.get('/legal_notice', function(req, res) {
-  i18n.setlang(req,res);
-  res.render('legal_notice', { title: 'Seller' });
-});
-
-router.get('/faq', function(req, res) {
-  i18n.setlang(req,res);
-  res.render('faq', { title: 'Seller' });
-});
-
-router.get('/tac', function(req, res) {
-  i18n.setlang(req,res);
-  res.render('tac', { title: 'Seller' });
-});
 
 router.get('/special_offer', function(req, res) {
   i18n.setlang(req,res);
   res.render('special_offer', { title: 'Seller' });
 });
 
-router.get('/register', function(req, res) {
+router.get('/registration', function(req, res) {
   i18n.setlang(req,res);
-  res.render('register', { title: 'Seller' });
+  res.render('registration', { title: 'Registration' });
 });
 
 router.get('/product_summary', function(req, res) {
@@ -67,14 +86,26 @@ router.get('/product_details', function(req, res) {
   i18n.setlang(req,res);
   res.render('product_details', { title: 'Seller' });
 });
-
-router.get('/companyInfo', function(req, res) {
+router.get('/addCity', function(req, res) {
   i18n.setlang(req,res);
-  res.render('companyInfo', { title: 'companyInfo' });
+  res.render('addCity', { title: 'addCity' });
+});
+router.get('/addMahalla', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('addMahalla', { title: 'addMahalla' });
+});
+router.get('/addArea', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('addArea', { title: 'addArea' });
+});
+router.get('/addSchool', function(req, res) {
+  i18n.setlang(req,res);
+  res.render('addSchool', { title: 'addSchool' });
 });
 
-router.get('/:locale', function(req, res) {
-  res.redirect("/seller");
+// set a cookie to requested locale
+router.get('/:locale', function (req, res) {
+  res.redirect("/user");
 });
 
 module.exports = router;
